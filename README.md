cdc02977a4eb99af8024b9cb03714bd7b4683f48
quereste <t.szczepanik@student.uj.edu.pl>
2025-04-22 23:22:04 +0200

    Improve README

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # VisionDepthNet
-3D distance estimation using monocular vision
 
-# Depth Estimation Docker
+The main lesson's goal is to introduce methos of 3D distance estimation using monocular vision. In particular the focus is on recognition of pedestrians using dashcam-like input to assist autonomous vehicles in making velocity reduction decisions.
+
+The implemented idea is effectively a pipeline that utilizes YOLO and MiDaS solutions to:
+- first: detect and segment parts of image that include pedestrians
+- second: create a depth map and extract distance in meters between photo taker and subsequent people in the picture
+
+Repository consists of following files:
+- depth_estimation.py - proposed pipeline to one-shot estimate distance to pedestrians from the camera perspective
+- car_copilot.py - imitation of real-life usage of depth estimation to estimate braking distance length
+- Dockerfile - configuration file to run the depth_estimation.py in dockerized way
+- requirements.txt - set of module requirements
+- testjpg.jpg - sample image (source)[]
+- testvideo.mp4 - sample video (source)[https://videos.pexels.com/video-files/5921059/5921059-uhd_3840_2160_30fps.mp4]
+
+
+# Depth Estimation Docker instruction
 
 ## Quick Start
 ```bash
@@ -18,6 +32,18 @@ docker run -it --rm \
   python depth_estimation.py --image images/yourphoto.jpg
 ```
 
+You can refrain from using Docker. Install requirements.txt and run depth_estimation or car_copilot e.g.:
+```bash
+pip install -r requirements.txt
+
+python depth_estimation.py --image images/yourphoto.jpg
+
+python car_copilot.py
+```
+
+For lightweight version of depth_estimation, replace **DPT_Large** with **MiDaS_small**.
+
 ## Requirements
-- Docker installed
+- packages listed in requirements.txt
+- Docker installed (optional)
 - NVIDIA Container Toolkit (for GPU support)
---
+++ car_copilot.py
@@ -0,0 +1,83 @@
+import os
+import random
+from depth_estimation import *
+
+FPS = 30
+FRAME_SIZE = (640, 384)
+
+def road_to_stop(velocity, deceleration=9.8, tire_friction=0.7):   
+    effective_deceleration = deceleration * tire_friction
+
+    return (velocity ** 2) / (2 * effective_deceleration)
+
+def press_brake():
+    print("Pressing the brake...")
+
+def process_image(frame):
+    frame = cv2.resize(frame, FRAME_SIZE)
+    detections, image = detect_and_segment(frame)
+    original_shape = image.shape[:2]
+    
+    depth_model, depth_transforms = setup_depth_estimator()
+    
+    depth_map = estimate_depth(image, depth_model, depth_transforms)
+    depth_map_meters = depth_map * SCALE_FACTOR
+    
+    detections = calculate_distances(detections, depth_map_meters, original_shape)
+
+    min_distance = float('inf')
+
+    for det in detections:
+        if det["class"] == "person" and det["distance"] is not None:
+            print(f"Detected {det['class']} at distance: {det['distance']:.2f} m")
+            min_distance = min(det["distance"], min_distance)
+
+    if min_distance == float('inf'):
+        print("INFO: no person detected.")
+    
+    return min_distance
+
+
+def car_copilot(dashcam_video_stream):
+    context = 50
+    distances = [0.0] * context
+    current_index = 0
+    frame_count = 0
+
+    cap = cv2.VideoCapture(dashcam_video_stream)
+    if not cap.isOpened():
+        raise ValueError(f"Unable to open video file: {dashcam_video_stream}")
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        temp_frame_path = "temp.jpg"
+        cv2.imwrite(temp_frame_path, frame)
+
+        current_distance = process_image(frame)
+
+        if current_index < context:
+            distances[current_index] = current_distance
+        else:
+            velocity = (current_distance - distances[current_distance]) * FPS
+            road_to_stop_distance = road_to_stop(velocity)
+            print(f"Velocity: {velocity:.2f} m/s, Road distance to stop: {road_to_stop_distance:.2f} m")
+
+            if road_to_stop_distance > current_distance:
+                print("Car is not in a safe distance to stop.")
+                press_brake()
+
+        current_index = (current_index + 1) % context    
+        frame_count += 1
+        print(f"Frame: {frame_count}, Distance: {current_distance:.2f} m")
+    cap.release()
+    return frame_count
+
+if __name__ == "__main__":
+    dashcam_video_stream = "./testvideo.mp4"  # Replace with your movie path
+    try:
+        car_copilot(dashcam_video_stream)
+    except Exception as e:
+        print(f"An error occurred: {e}")
